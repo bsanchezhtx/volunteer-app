71070b2a1fdbf0112f216e7bed114c00b7ad344d
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-04 13:36:15 -0500

    skills.ts

---
+++ src/data/skills.ts
@@ -0,0 +1,6 @@
+export default [
+  { value: 'communication', label: 'Communication' },
+  { value: 'leadership',    label: 'Leadership' },
+  { value: 'teamwork',      label: 'Teamwork' },
+  { value: 'firstAid',      label: 'First Aid' },
+];
