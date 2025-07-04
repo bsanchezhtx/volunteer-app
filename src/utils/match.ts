d1fc0a3768143ffb59dd743731c895a9a5d53227
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-04 18:22:49 -0500

    match.ts

--- src/utils/match.ts
+++ src/utils/match.ts
@@ -1,13 +1,8 @@
-import { Volunteer, Event } from '../types';
+import { Volunteer, Event } from "../types";
+export const score = (vol: Volunteer, evt: Event) =>
+  evt.requiredSkills.filter(s => vol.skills.includes(s)).length +
+  (vol.availability.includes(evt.date) ? 1 : 0) +
+  evt.urgency;
+export const bestEventFor = (v: Volunteer, es: Event[]) =>
+  es.map(e => ({ e, s: score(v, e) })).sort((a, b) => b.s - a.s)[0]?.e;
 
-export function score(vol: Volunteer, evt: Event) {
-  const skillMatch = evt.requiredSkills.filter(s => vol.skills.includes(s)).length;
-  const dateMatch  = vol.availability.includes(evt.date) ? 1 : 0;
-  return skillMatch + dateMatch + evt.urgency;
-}
-
-export function bestEventFor(vol: Volunteer, events: Event[]) {
-  return events
-    .map(evt => ({ evt, s: score(vol, evt) }))
-    .sort((a, b) => b.s - a.s)[0]?.evt;
-}
