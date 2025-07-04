75d13f8483509cee678a2ee5bc05e02f9c33d31e
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-04 13:35:42 -0500

    usStates.ts

---
+++ src/data/usStates.ts
@@ -0,0 +1,5 @@
+export default [
+  { value: 'AL', label: 'Alabama' }, { value: 'AK', label: 'Alaska' },
+  { value: 'AZ', label: 'Arizona' }, { value: 'AR', label: 'Arkansas' },
+  { value: 'WI', label: 'Wisconsin' }, { value: 'WY', label: 'Wyoming' },
+];
