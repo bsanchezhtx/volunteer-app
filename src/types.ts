8a0ca51e184c29db593a9b3cf76a103994f5ed2c
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-04 13:25:50 -0500

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Volunteer = {
+  id: string;
+  name: string;
+  skills: string[];
+  availability: string[];   
+};
+
+export type Event = {
+  id: string;
+  name: string;
+  requiredSkills: string[];
+  date: string;
+  urgency: number;        
+};
