3b702c7a731cf56ee282bf8cc44ba4d719b2aa86
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-18 23:26:44 -0500

    matcher.js

---
+++ backend/src/utils/matcher.js
@@ -0,0 +1,10 @@
+export const score = (vol, evt) =>
+  evt.requiredSkills.filter(s => vol.skills.includes(s)).length +
+  (vol.availability.includes(evt.date) ? 1 : 0) +
+  evt.urgency;
+
+export const bestEventFor = (vol, evts) =>
+  evts
+    .map(e => ({ e, s: score(vol, e) }))
+    .sort((a, b) => b.s - a.s)[0]?.e;
+
