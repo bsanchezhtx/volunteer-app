af060253823d5f440d9327d4de89631bc8afa98a
savar-p <108385725+savar-p@users.noreply.github.com>
2025-07-31 12:56:17 -0500

    Delete backend/src/routes/refresh.js

--- backend/src/routes/refresh.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import refreshTokenHandler from "../controllers/refreshTokenController.js";
-
-const router = express.Router();
-router.get("/", refreshTokenHandler);
-
-export default router;
