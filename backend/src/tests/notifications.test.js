7d5d7e95b889069bdc87d852e4dacbd4cada1adf
savar-p <108385725+savar-p@users.noreply.github.com>
2025-08-01 16:46:09 -0500

    notifications.test.js

--- backend/src/tests/notifications.test.js
+++ backend/src/tests/notifications.test.js
@@ -1,14 +1,13 @@
-import request from "supertest";
-import app from "../index.js";
-import { sign } from "../middleware/auth.js";
-import prisma from "../prisma.js";
+const request = require("supertest");
+const app = require("../index.js");
+const { sign } = require("../middleware/auth.js");
+const prisma = require("../prisma.js").default;
 
 let token;
 
 beforeAll(async () => {
   const user = await prisma.user.findFirst();
   token = sign(user);
-
   await prisma.notification.create({
     data: { userId: user.id, text: "Test notification" }
   });
@@ -21,5 +20,3 @@ it("fetches notifications", async () => {
   expect(res.statusCode).toBe(200);
   expect(Array.isArray(res.body)).toBe(true);
 });
-
-
