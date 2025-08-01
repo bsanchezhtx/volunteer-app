4881cd456763c8cca99ba30f1cced9a6c388ce90
savar-p <108385725+savar-p@users.noreply.github.com>
2025-08-01 17:00:57 -0500

    events.test.js

--- backend/src/tests/events.test.js
+++ backend/src/tests/events.test.js
@@ -1,7 +1,7 @@
-const request = require("supertest");
-const app = require("../index.js");
-const { sign } = require("../middleware/auth.js");
-const prisma = require("../prisma.js").default;
+import request from "supertest";
+import app from "../index.js";
+import { sign } from "../middleware/auth.js";
+import prisma from "../prisma.js";
 
 let token;
 
